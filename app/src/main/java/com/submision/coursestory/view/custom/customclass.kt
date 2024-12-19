f5a8a39409fe69627f86b9c9af2dc4ea950d0447
atzulf <atzulfikar45@gmail.com>
2024-12-20 00:28:33 +0700

    test

--- app/src/main/java/com/submision/coursestory/view/custom/customclass.kt
+++ app/src/main/java/com/submision/coursestory/view/custom/customclass.kt
@@ -14,6 +14,7 @@ import com.submision.coursestory.R
 
 class MyPassword : AppCompatEditText, View.OnTouchListener {
 
+//    halo
     private lateinit var clearButtonImage: Drawable
 
     constructor(context: Context) : super(context) {
