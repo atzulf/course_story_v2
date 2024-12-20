e080b980493b5b89c39ad6993d571b435f000dc7
atzulf <atzulfikar45@gmail.com>
2024-12-20 21:11:58 +0700

    serupa

--- app/src/main/java/com/submision/coursestory/data/api/ApiService.kt
+++ app/src/main/java/com/submision/coursestory/data/api/ApiService.kt
@@ -35,7 +35,9 @@ interface ApiService {
 
     @GET("stories")
     suspend fun getStories(
-        @Header("Authorization") token: String
+        @Header("Authorization") token: String,
+        @Query("page") page: Int,
+        @Query("size") size: Int
     ): AllStoriesResponse
 
     @GET("stories/{id}")
