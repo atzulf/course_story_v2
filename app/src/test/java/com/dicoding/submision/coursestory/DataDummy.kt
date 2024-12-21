91bd689e95a184d058cdc3caf2159a356bffd0e9
atzulf <atzulfikar45@gmail.com>
2024-12-21 12:30:48 +0700

    add DataDummy.kt

---
+++ app/src/test/java/com/dicoding/submision/coursestory/DataDummy.kt
@@ -0,0 +1,22 @@
+package com.dicoding.submision.coursestory
+
+import com.submision.coursestory.data.response.ListStoryItem
+
+object DataDummy {
+    fun generateDummyStoryResponse(): List<ListStoryItem> {
+        val items: MutableList<ListStoryItem> = arrayListOf()
+        for (i in 0..100) {
+            val story = ListStoryItem(
+                id = i.toString(),
+                name = "Name $i",
+                description = "Description $i",
+                createdAt = "Created At $i",
+                photoUrl = "photoUrl $i",
+                lon = i.toDouble(),
+                lat = i.toDouble()
+            )
+            items.add(story)
+        }
+        return items
+    }
+}
